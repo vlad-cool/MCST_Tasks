1d10dcab2040828c3442d841ecbe034c41f5fe6a
vlad-cool <vlad-matveev2004@mail.ru>
2024-10-03 14:09:23 +0300

    log2 simplified

--- log2.v
+++ log2.v
@@ -4,14 +4,14 @@ module decoder
     output wire[2:0] out
 );
 
-assign out = ({in[0], in[0], in[0], in[0],  in[0], in[0], in[0], in[0]} & 0) | 
-             ({in[1], in[1], in[1], in[1],  in[1], in[1], in[1], in[1]} & 1) |
-             ({in[2], in[2], in[2], in[2],  in[2], in[2], in[2], in[2]} & 2) |
-             ({in[3], in[3], in[3], in[3],  in[3], in[3], in[3], in[3]} & 3) |
-             ({in[4], in[4], in[4], in[4],  in[4], in[4], in[4], in[4]} & 4) |
-             ({in[5], in[5], in[5], in[5],  in[5], in[5], in[5], in[5]} & 5) |
-             ({in[6], in[6], in[6], in[6],  in[6], in[6], in[6], in[6]} & 6) |
-             ({in[7], in[7], in[7], in[7],  in[7], in[7], in[7], in[7]} & 7);
+assign out = ({8{in[0]}} & 0) | 
+             ({8{in[1]}} & 1) |
+             ({8{in[2]}} & 2) |
+             ({8{in[3]}} & 3) |
+             ({8{in[4]}} & 4) |
+             ({8{in[5]}} & 5) |
+             ({8{in[6]}} & 6) |
+             ({8{in[7]}} & 7);
 
 endmodule
 
